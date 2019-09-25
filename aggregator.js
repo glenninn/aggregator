68eb0d5d9412797d19f75891e5c5f20fb932bacd
glenninn <glenninn@yahoo.com>
2019-09-24 18:16:29 -0700

    Modify to optionally use Stage Servers

--- aggregator.js
+++ aggregator.js
@@ -4,28 +4,56 @@ const { exec } = require('child_process');
 
 var auth = authorizer();
 var useEditor = false;
+var useStage  = false;
+
+//
+// Set the shell command to invoke your favorite JSON/text editor
+//
+var editorCommand = "npp";
+
+
 
 function doHelp() {
-	console.log("Usage: node aggregator.js {Numeric Id}  {-e}");
+	console.log("Usage: node aggregator.js {Numeric Id}  {-e | -a}");
 	console.log("  Retrieve and display the aggregated BlueJeans Meeting Information for the specified meeting");
 	console.log("Options:");
 	console.log("  -e : open file in editor instead of browser");
+	console.log("  -a : Send to BlueJeans Stage");
 	process.exit();
 }
+
+
+function jss(obj){
+	return JSON.stringify(obj,null,2);
+}
+
+
 
 if(process.argv.length < 3) doHelp;
 
 var mtgId = process.argv[2];
-var opt   = process.argv[3];
+if(!mtgId || (mtgId == '?')) doHelp();
 
-if(mtgId == '?') doHelp();
-if(opt == "-e" ) useEditor = true;
+for(var o= 3; o< process.argv.length; o++){
+	var opt = process.argv[o];
+	switch (opt) {
+		case "-e" : useEditor = true;
+		break;
+		case "-a" :
+		case "-s" : useStage = true;
+		break;
+		default :
+		   console.log("?Unrecognized option: " + opt);
+		   break;
+	}
+}
+
 
 /*
-https://bluejeans.com/seamapi/v1/services/aggregator/meeting?pairing=none&allowGeoLocation=true&user_access_token=d0f1262b636543958176933f5a909e6f
+https://bluejeans.com/seamapi/v1/services/aggregator/meeting?pairing=none&allowGeoLocation=true&user_access_token=d0...a909e6f
 
 */
-var apiHost = "api.bluejeans.com";
+var apiHost = (useStage ?  "api.a.bluejeans.com" : "api.bluejeans.com");
 var url = "/v1/services/aggregator/meeting?pairing=none&allowGeoLocation=true";
 var arec = {
    attributes: [],
@@ -33,13 +61,17 @@ var arec = {
    meetingPasscode : ""
 };
 
+console.log("Sending 'aggregator' API to: " + apiHost);
+
+
 auth.post(apiHost,url,arec).then(
 	(ok)=>{
-		console.log("Aggregate Meeting Data:\n" + JSON.stringify(ok,null,2));
+		var snippet = jss(ok);
+		console.log("Aggregate Meeting Data:\n" + snippet.slice(0,80) + "...");
 		var tfolder = process.env.TEMP;
 		var tfn  = Date.now().toString() + ".json";
 		var fn = tfolder + "\\" + tfn;
-		fs.writeFile(fn, JSON.stringify(ok,null,2),
+		fs.writeFile(fn, snippet,
 			(err)=>{
 				if(err){
 					console.log("Error writing file: " + fn);
@@ -52,7 +84,7 @@ auth.post(apiHost,url,arec).then(
 					exec(execCmd, (err, stdout, stderr) => {
 					  if (err) {
 						// node couldn't execute the command
-						console.log("Couldn't execute thje 'cmd' app");
+						console.log("Couldn't execute the 'cmd' app");
 					  }
 					});					
 				}
